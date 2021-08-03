f9df0cbb1596ef5aec547c94dbbb7f12da9d05ec
chrisgrounds <chrisbacon2009@hotmail.com>
2021-08-03 13:10:12 +0200

    justify text

--- components/Information.js
+++ components/Information.js
@@ -15,7 +15,7 @@ const Information = ({ valuation, discountRate }) => {
 
   return (
     <div style={{ marginBottom: "1rem", maxWidth: "800px" }}>
-      <p>
+      <p style={{ textAlign: "justify" }}>
         Based on this DCF valuation of <EmphasiseText color={color} text={`\$${dcfValue}`} /> and current stock price of <EmphasiseText color={color} text={`\$${currentPrice}`} />, you could <EmphasiseText color={color} text={buyOrSell} /> this equity and expect a <EmphasiseText color={color} text={returnExpectation} /> followed by returns in line with your discount rate of <EmphasiseText color={color} text={`${discountRate * 100 || 15}%`} />.
       </p>
     </div>
